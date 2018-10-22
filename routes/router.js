2788ba82c66f2597903ec3f03fad16b8fecd56b3
postlude <seungdeokhan@naver.com>
2018-10-22 20:55:00 +0900

    검색 기능 구현 중

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var moment = require('moment-timezone');
+var request = require('request');
+var cheerio = require('cheerio');
+var iconv = require('iconv-lite');
 var router = express.Router();
 
 router.get('/', function(req, res){
@@ -27,5 +30,54 @@ router.post('/password/ajax_check', function(req, res){
     }
 });
 
+// google search
+router.get('/search', function(req, res){
+    res.render('search');
+});
+
+router.post('/search', function(req, res){
+    var url = 'https://www.google.co.kr/search?q=' + req.body.search_value;
+    request(url, function(error, response, html){  
+        if (error) {
+            throw error
+        };
+
+        const $ = cheerio.load(html);
+        let searchResult = $('#search').html();
+        
+        // console.log($('div').html());
+        // console.log(searchResult);
+        // res.send(searchResult);
+        // res.send(cheerio.load(html));
+        res.render('searchResult', {'searchResult' : searchResult});
+    });
+    // res.send(url);
+});
+
+router.post('/search/ajax_search', function(req, res){
+    var url = 'https://www.google.co.kr/search?q=' + req.body.searchValue;
+    request(url, function(error, response, html){  
+        if (error) {
+            throw error
+        };
+
+        var strContents = new Buffer(html);
+        console.log(iconv.decode(strContents, 'MS949').toString());
+
+        const $ = cheerio.load(html);
+        let searchResult = $('#search').html();
+        
+        
+        // console.log($('div').html());
+        // console.log(searchResult);
+        res.send(searchResult);
+        // res.send(cheerio.load(html));
+        // res.render('searchResult', {'searchResult' : searchResult});
+    });
+    // res.send(url);
+});
+
+// 참고
+// https://www.youtube.com/watch?v=V3qibfw_5nQ
 
 module.exports = router;
