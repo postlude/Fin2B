51d34f079ce039d1f8a6f7d84e83058b1a3a3e6f
postlude <seungdeokhan@naver.com>
2018-10-22 17:40:48 +0900

    비밀번호 폼 완료, 서버 시간 ajax url 변경

--- routes/router.js
+++ routes/router.js
@@ -3,16 +3,28 @@ var moment = require('moment-timezone');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('test', {title: 'Test Title', content: 'world'});
+    res.redirect('password');
 });
 
+router.get('/server_date/ajax_get', function(req, res){
+    var serverTime = moment(new Date()).tz('Asia/Seoul').format('YYYY년 MM월 DD일 HH시 mm분 ss초');
+    res.send(serverTime);
+});
+
+// password
 router.get('/password', function(req, res){
     res.render('password');
 });
 
-router.get('/ajax/get_server_date', function(req, res){
-    var serverTime = moment(new Date()).tz('Asia/Seoul').format('YYYY년 MM월 DD일 HH시 mm분 ss초');
-    res.send(serverTime);
+router.post('/password/ajax_check', function(req, res){
+    var password = req.body.pwd;
+    var reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&+=]).{8,}$/;
+    // var reg = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&+=]).{8,}$');
+    if(reg.test(password)){
+        res.send(true);
+    }else{
+        res.send(false);
+    }
 });
 
 
